1ec34bc642d76a5f47904c7e197f6477d52b61a2
tanvir <tanvirulislam149@gmail.com>
2023-07-31 22:46:46 +0600

    updateStatus done

--- src/app/modules/Orders/order.controller.js
+++ src/app/modules/Orders/order.controller.js
@@ -26,3 +26,12 @@ exports.getAllOrdersController = async (req, res, next) => {
     res.status(500).send({ message: error.message });
   }
 }
+
+exports.updateOrderStatusController = async (req, res, next) => {
+  try {
+    const result = await Orders.findByIdAndUpdate(req.body._id, { status: req.body.status });
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+}
--- src/app/modules/Orders/order.route.js
+++ src/app/modules/Orders/order.route.js
@@ -5,5 +5,6 @@ const OrderController = require("./order.controller");
 router.post("/addOrder", OrderController.addOrderController)
 router.get("/getMyCourses", OrderController.getMyOrdersController)
 router.get("/getAllOrders", OrderController.getAllOrdersController)
+router.post("/updateOrderStatus", OrderController.updateOrderStatusController)
 
 module.exports = router;
